ecc126d2c72d822ca3934d9256d692660df48634
k1dd1e <102634623+k1dd1e@users.noreply.github.com>
2023-12-04 10:48:03 +0000

    123

--- 0_variables/variables.py
+++ 0_variables/variables.py
@@ -1,37 +1,38 @@
 def declaration():
     # Объяви переменную x, равную 10
     #!v
-
+    x = 10
     #!^
     return x
 
 def multi_declaration():
     # На одной строчке объяви переменные a, b, c и d, равные 10, 20, 30 и 40 соответственно
     #!v
-
+    a, b, c, d = 10, 20, 30, 40 
     #!^
     return (a, b, c, d)
 
 def bools():
     # Объяви переменную TRUEE равную истине (True)
     #!v
-
+    TRUEE = True
     #!^
     return TRUEE
 
 def types():
     # Объяви три переменных: var_int, var_float и var_bool так, чтобы они были типов int, float и bool соответственно
     #!v
-    var_int = x
-    var_float = x
-    var_bool = x
+    x = 10
+    var_int = int(x)
+    var_float = float(x)
+    var_bool = bool(x)
     #!^
     return (type(var_int), type(var_float), type(var_bool))
 
 def strings():
     # Создай строку hello_world с текстом "Hello world!"
     #!v
-
+    hello_world = str("Hello world!")
     #!^
     return hello_world
 
@@ -42,7 +43,7 @@ def string_concat():
     # Используя уже объявленные строки и операцию конкатенации
     #  создай строку hello_world с текстом "Hello world!"
     #!v
-
+    hello_world = hello + space + world
     #!^
     return hello_world
 
@@ -50,7 +51,7 @@ def string_convertion():
     a = 123
     # Преобразуй переменную a в строку a_str с соответствующим значением с помощью str()
     #!v
-    
+    a_str = str(a)
     #!^
     return a_str
 
@@ -60,13 +61,13 @@ def calculator():
     #  частное, частное (при целочисленном делении),
     #  остаток от деления, степень и сохрани их в соответствующие переменные
     #!v
-    sum = x # сумма
-    dif = x # разность
-    mul = x # произведение
-    divf = x # частное (вещественное деление)
-    divi = x # частное (целочисленное деление)
-    rem = x # остаток от деления
-    pow = x # степень
+    sum = a+b # сумма
+    dif = a-b # разность
+    mul = a*b # произведение
+    divf = a/b # частное (вещественное деление)
+    divi = a//b # частное (целочисленное деление)
+    rem = a%b # остаток от деления
+    pow = a**b # степень
     #!^
     return (sum, dif, mul, divf, divi, rem, pow)
 
